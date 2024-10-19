b2f999bfb98c987652e8a20482d263e1d2a42a75
Subhan Asghar <subhanasghar16@gmail.com>
2024-10-19 17:18:51 +0500

    Error fix

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -64,10 +64,9 @@ const Home = () => {
         <button onClick={() => setCategory('politics')} className="bg-white w-24 h-10 rounded-full text-red-600 font-semibold shadow hover:bg-red-500 hover:text-white transition duration-300">
             Politics
         </button>
-        <svg onClick={setuser(false)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white ml-4 cursor-pointer hover:text-gray-300 transition duration-300">
-            
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
-        </svg>
+        <svg onClick={() => setuser(false)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white ml-4 cursor-pointer hover:text-gray-300 transition duration-300">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
+                    </svg>
     </div>
 </nav>
 
